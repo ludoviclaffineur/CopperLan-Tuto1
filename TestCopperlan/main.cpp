d06015a9a53515b0d36eb15c8b3e0f4ec5844c42
Ludovic LAFFINEUR <ludovic.laffineur@gmail.com>
2015-07-10 15:43:21 +0200

    Initial Commit

---
+++ TestCopperlan/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TestCopperlan
+//
+//  Created by Ludovic Laffineur on 10/07/15.
+//  Copyright (c) 2015 Ludovic Laffineur. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
